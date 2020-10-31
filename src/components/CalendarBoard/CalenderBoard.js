a97cfdce747dcd779d3d239a666a6384b28e6497
YukiSugaya <sugaya324@gmail.com>
2020-10-30 17:20:02 -0700

    ADD: CalendarBoard

---
+++ src/components/CalendarBoard/CalenderBoard.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { GridList, Typography, makeStyles } from '@material-ui/core';
+import { CalendarElement } from '../CalendarElement/CalenderElement';
+
+const days = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
+
+const useStyles = makeStyles({
+  container: {
+    height: '90vh',
+  },
+  grid: {
+    'border-top': '1px solid #ccc',
+    'border-left': '1px solid #ccc',
+  },
+  days: {
+    'border-right': '1px solid #ccc',
+    color: '#aaa',
+  },
+});
+
+export const CalendarBoard = ({ calendar }) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <GridList className={classes.grid} cols={7} spacing={0} cellHeight="auto">
+        {days.map((d) => (
+          <li key={d}>
+            <Typography className={classes.days} align="center" component="div">
+              {d}
+            </Typography>
+          </li>
+        ))}
+        {calendar.map((day) => (
+          <li key={day}>
+            <CalendarElement day={day.date()} />
+          </li>
+        ))}
+      </GridList>
+    </div>
+  );
+};
