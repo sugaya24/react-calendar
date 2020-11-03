cb53e86ad14ec87123524930ac0cef4c6206505b
YukiSugaya <sugaya324@gmail.com>
2020-11-03 01:09:34 -0800

    ADD: CalendarBoard container

--- src/components/CalendarBoard/container.js
+++ src/components/CalendarBoard/container.js
@@ -2,20 +2,47 @@ import { connect } from 'react-redux';
 
 import { CalendarBoard } from './CalenderBoard';
 import { createCalendar } from '../../utils/calendar';
+import {
+  addScheduleOpenDialog,
+  addScheduleSetValue,
+} from '../../redux/addSchedule/actions';
+
+import { setSchedules } from '../../utils/schedule';
 
 const mapStateToProps = (state) => {
   return {
     calendar: state.calendar,
+    schedules: state.schedules,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    openAddScheduleDialog: (d) => {
+      dispatch(addScheduleOpenDialog());
+      dispatch(addScheduleSetValue({ date: d }));
+    },
   };
 };
 
 const mergeProps = (stateProps, dispatchProps) => {
+  const {
+    calendar: month,
+    schedules: { items: schedules },
+  } = stateProps;
+
+  const calendar = setSchedules(createCalendar(month), schedules);
+
   return {
     ...stateProps,
     ...dispatchProps,
-    month: stateProps.calendar,
-    calendar: createCalendar(stateProps.calendar),
+    calendar,
+    month,
   };
 };
 
-export default connect(mapStateToProps, null, mergeProps)(CalendarBoard);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+  mergeProps
+)(CalendarBoard);
