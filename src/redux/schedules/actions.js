d5d33d8b836ea60adc575625745121bcec54f5e9
YukiSugaya <sugaya324@gmail.com>
2020-11-03 01:10:28 -0800

    Fix: Delete console.log

--- src/redux/schedules/actions.js
+++ src/redux/schedules/actions.js
@@ -1,7 +1,6 @@
 export const SCHEDULE_ADD_ITEM = 'SCHEDULE_ADD_ITEM';
 
 export const scheduleAddItem = (payload) => {
-  console.log('payload', payload);
   return {
     type: SCHEDULE_ADD_ITEM,
     payload,
