f8b9bfa3ecd0c2b4dc4cf0353c728950cf587dc4
YukiSugaya <sugaya324@gmail.com>
2020-11-04 02:18:44 -0800

    MOD: Change text color except to current month

--- src/components/CalendarElement/CalenderElement.js
+++ src/components/CalendarElement/CalenderElement.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Schedule } from '../Schedule/Schedule';
 import { Typography, makeStyles } from '@material-ui/core';
 
-import { isFirstDay } from '../../utils/calendar';
+import { isFirstDay, getMonth, isSameMonth } from '../../utils/calendar';
 
 const useStyles = makeStyles({
   date: {
     'border-right': '1px solid #ccc',
+    color: (props) => props.color,
   },
   schedule: {
     'border-bottom': '1px solid #ccc',
@@ -22,7 +23,10 @@ const createSchedule = (day) => {
 export const CalendarElement = ({ day, month, schedules, calendar }) => {
   const format = isFirstDay(day) ? 'MMM D' : 'D';
 
-  const props = { cellHeight: calendar.length <= 35 ? '15vh' : '12vh' };
+  const props = {
+    cellHeight: calendar.length <= 35 ? '15vh' : '12vh',
+    color: isSameMonth(day, getMonth(month)) ? '#000' : '#ccc',
+  };
   const classes = useStyles(props);
 
   return (
